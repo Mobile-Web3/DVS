af24fb1e3868d998a49a00998f0db1dfb74e0f44
qqdasdzxc <hook23@mail.ru>
2022-07-05 16:18:52 +0300

    android: add header to main screen with mock items in 2 columns

--- androidApp/src/main/java/com/mobileweb3/dvs/android/screens/main/MainScreen.kt
+++ androidApp/src/main/java/com/mobileweb3/dvs/android/screens/main/MainScreen.kt
@@ -1,52 +1,36 @@
 package com.mobileweb3.dvs.android.screens.main
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.mobileweb3.dvs.app.MainAction
+import com.mobileweb3.dvs.android.R
 import com.mobileweb3.dvs.app.MainStore
 
 @Composable
 fun MainScreen(
-    store: MainStore
+    store: MainStore,
+    modifier: Modifier = Modifier
 ) {
-    val state = store.observeState().collectAsState()
-
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 24.dp)
     ) {
-        Box(
+        Image(
+            painter = painterResource(id = R.drawable.dvs_background),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+        )
+        ValidatorsList(
+            store = store,
             modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "Count: ${state.value.count}",
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .wrapContentHeight()
-                    .clickable { store.dispatch(MainAction.Click) },
-            )
-        }
-
-        Text(
-            text = "by Mobile Web3",
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(16.dp)
-                .clickable { store.dispatch(MainAction.Click) }
+                .fillMaxSize()
         )
     }
 }
---
+++ androidApp/src/main/java/com/mobileweb3/dvs/android/screens/main/ValidatorsList.kt
@@ -0,0 +1,85 @@
+package com.mobileweb3.dvs.android.screens.main
+
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.dp
+import com.mobileweb3.dvs.app.MainStore
+
+@Composable
+fun ValidatorsList(
+    store: MainStore,
+    columns: Int = 2,
+    modifier: Modifier = Modifier,
+    scrollState: ScrollState = rememberScrollState(),
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+    val state = store.observeState().collectAsState()
+
+    val mockList: List<Int> = (1..100).toList()
+
+    val chunkedList = remember(mockList, columns) {
+        mockList.chunked(columns)
+    }
+
+    Column(
+        modifier = modifier.verticalScroll(scrollState)
+    ) {
+        chunkedList.forEach { chunk ->
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+            ) {
+
+                chunk.forEach { model ->
+                    Text(
+                        text = model.toString(),
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .weight(1f)
+                    )
+                    //                    AnimalCard(
+                    //                        animal = animal,
+                    //                        modifier = Modifier
+                    //                            .padding(2.dp)
+                    //                            .weight(1f)
+                    //                    )
+                }
+
+                val emptyCells = columns - chunk.size
+                if (emptyCells > 0) {
+                    Spacer(modifier = Modifier.weight(emptyCells.toFloat()))
+                }
+
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .width(contentPadding.calculateEndPadding(LocalLayoutDirection.current))
+                )
+            }
+        }
+    }
+}
