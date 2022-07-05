697505b2c4f8616d424ae963a8508b9d480d9f92
qqdasdzxc <hook23@mail.ru>
2022-07-05 11:05:40 +0300

    add first version of validator data model

---
+++ shared/src/commonMain/kotlin/com/mobileweb3/dvs/core/entity/Validator.kt
@@ -0,0 +1,55 @@
+package com.mobileweb3.dvs.core.entity
+
+data class Validator(
+    val title: String,
+    val avatar: String,
+    val description: String,
+    val mainNets: List<ValidatorNetwork>,
+    val testNets: List<ValidatorNetwork>? = null,
+    val otherProjects: List<OtherProjects>,
+    val ambassadorPrograms: List<Project>,
+    val contributions: List<Contributions>,
+    val contacts: List<Contact>,
+    val otherInfo: List<OtherInfo>
+)
+
+data class ValidatorNetwork(
+    val network: BlockChainNetwork,
+    val link: String
+)
+
+data class BlockChainNetwork(
+    val title: String,
+    val image: String
+)
+
+data class OtherProjects(
+    val topic: String,
+    val projects: List<Project>
+)
+
+data class Project(
+    val title: String,
+    val link: String
+)
+
+data class Contributions(
+    val type: String, //tech,community,other
+    val contributions: List<Contribution>
+)
+
+data class Contribution(
+    val label: String,
+    val link: String? = null
+)
+
+data class Contact(
+    val type: String, //tg, twitter, keybase, github, website, medium, discord, instagram, email
+    val link: String
+)
+
+data class OtherInfo(
+    val title: String,
+    val body: String
+)
+
