a4b2e991d8e4c2919b7a676bbf365bffabdfaf9f
qqdasdzxc <hook23@mail.ru>
2022-07-09 09:19:59 +0300

    add linear gradient to validator card background

--- androidApp/src/main/java/com/mobileweb3/dvs/android/screens/main/ValidatorCard.kt
+++ androidApp/src/main/java/com/mobileweb3/dvs/android/screens/main/ValidatorCard.kt
@@ -1,5 +1,7 @@
 package com.mobileweb3.dvs.android.screens.main
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.height
@@ -8,11 +10,20 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.google.accompanist.placeholder.PlaceholderHighlight
@@ -22,15 +33,31 @@ import com.mobileweb3.dvs.android.utils.applyDefaults
 import com.mobileweb3.dvs.app.ValidatorViewState
 import com.skydoves.landscapist.CircularReveal
 import com.skydoves.landscapist.glide.GlideImage
+import com.skydoves.landscapist.palette.BitmapPalette
+
+private val AVATAR_HEIGHT_WIDTH = 100.dp
 
 @Composable
 fun ValidatorCard(
     validatorViewState: ValidatorViewState,
     modifier: Modifier
 ) {
+    var palette by remember { mutableStateOf<Palette?>(null) }
+
     Box(
         modifier = modifier
             .height(200.dp)
+            .border(width = 2.dp, color = Color.White, shape = RoundedCornerShape(10.dp))
+            .background(
+                brush = palette?.let { safePalette ->
+                    val mostThreeColors = safePalette.swatches.sortedByDescending { it.population }.take(2)
+                    return@let Brush.linearGradient(
+                        mostThreeColors.map { Color(it.rgb) }
+                    )
+                } ?: Brush.linearGradient(
+                    listOf(Color.Black, Color.Black)
+                )
+            )
     ) {
         if (validatorViewState.isLoading) {
             AvatarShimmer(Alignment.TopCenter)
@@ -39,8 +66,8 @@ fun ValidatorCard(
         GlideImage(
             modifier = Modifier
                 .align(alignment = Alignment.TopCenter)
-                .width(100.dp)
-                .height(100.dp),
+                .width(AVATAR_HEIGHT_WIDTH)
+                .height(AVATAR_HEIGHT_WIDTH),
             imageModel = validatorViewState.validatorModel?.avatar,
             requestBuilder = {
                 Glide.with(LocalContext.current.applicationContext)
@@ -54,6 +81,7 @@ fun ValidatorCard(
             loading = {
                 AvatarShimmer(Alignment.Center)
             },
+            bitmapPalette = BitmapPalette { palette = it }
         )
 
         Text(
@@ -80,9 +108,10 @@ fun BoxScope.AvatarShimmer(
 ) {
     Text(
         text = "default text",
-        modifier = Modifier.align(alignment)
-            .width(100.dp)
-            .height(100.dp)
+        modifier = Modifier
+            .align(alignment)
+            .width(AVATAR_HEIGHT_WIDTH)
+            .height(AVATAR_HEIGHT_WIDTH)
             .placeholder(
                 visible = true,
                 color = Color.Gray,
