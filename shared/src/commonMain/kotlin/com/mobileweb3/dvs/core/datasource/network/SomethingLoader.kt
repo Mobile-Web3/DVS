593722a22a2d43dbf91bcac3812e5310e9f7202f
qqdasdzxc <hook23@mail.ru>
2022-07-11 21:53:46 +0300

    shared: add network calls and json config fixes

--- shared/src/commonMain/kotlin/com/mobileweb3/dvs/core/datasource/network/SomethingLoader.kt
+++ shared/src/commonMain/kotlin/com/mobileweb3/dvs/core/datasource/network/SomethingLoader.kt
@@ -1,6 +1,8 @@
 package com.mobileweb3.dvs.core.datasource.network
 
 import com.mobileweb3.dvs.core.entity.SomethingResponse
+import com.mobileweb3.dvs.core.entity.proposal.Proposal
+import com.mobileweb3.dvs.core.entity.transaction.Transaction
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
@@ -11,7 +13,24 @@ class SomethingLoader(
     private val httpClient: HttpClient
 ) {
 
+    private val json = Json {
+        ignoreUnknownKeys = true
+        coerceInputValues = true
+    }
+
     suspend fun getSomething(url: String): SomethingResponse {
-        return Json.decodeFromString(httpClient.get(url).bodyAsText())
+        return json.decodeFromString(httpClient.get(url).bodyAsText())
+    }
+
+    suspend fun getProposalsFromMintScan(chain: String): List<Proposal> {
+        return json.decodeFromString(httpClient.get("https://api.mintscan.io/v1/$chain/proposals").bodyAsText())
+    }
+
+    //cosmos1vvwtk805lxehwle9l4yudmq6mn0g32pxqjlrmt
+    suspend fun getValidatorTransactions(validationAddress: String): List<Transaction> {
+        return json.decodeFromString(
+            httpClient.get("https://api.cosmostation.io/v1/account/new_txs/$validationAddress?limit=100&from=0")
+                .bodyAsText()
+        )
     }
 }
