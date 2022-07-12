44bd9e99a35df14b86ba6f470a516ab5389cb490
qqdasdzxc <hook23@mail.ru>
2022-07-12 07:49:49 +0300

    android: remove light colored theme

--- androidApp/src/main/java/com/mobileweb3/dvs/android/ui/AppTheme.kt
+++ androidApp/src/main/java/com/mobileweb3/dvs/android/ui/AppTheme.kt
@@ -1,24 +1,18 @@
 package com.mobileweb3.dvs.android.ui
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val PrimaryColor = Color(0xff735095)
 
-private val LightColors = lightColors(
-    primary = PrimaryColor,
-    background = Color.Black,
-    onBackground = Color.White
-)
 private val DarkColors = darkColors(
     primary = PrimaryColor,
     onSecondary = Color.White,
-    background = Color.Black
+    background = Color.Black,
+    onPrimary = Color.White
 )
 
 @Composable
@@ -26,7 +20,7 @@ fun AppTheme(
     content: @Composable () -> Unit
 ) {
     MaterialTheme(
-        colors = if (isSystemInDarkTheme()) DarkColors else LightColors,
+        colors = DarkColors,
         content = {
             Surface(content = content)
         }
