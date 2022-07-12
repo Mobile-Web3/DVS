49d2746a226b39305cacadd79c3efc79df9542f5
qqdasdzxc <hook23@mail.ru>
2022-07-12 11:16:02 +0300

    android: add simple validator votes content view

--- androidApp/src/main/java/com/mobileweb3/dvs/android/screens/voting/ValidatorVotesContent.kt
+++ androidApp/src/main/java/com/mobileweb3/dvs/android/screens/voting/ValidatorVotesContent.kt
@@ -3,6 +3,7 @@ package com.mobileweb3.dvs.android.screens.voting
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -10,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.mobileweb3.dvs.app.ValidatorVotesStore
 import com.mobileweb3.dvs.core.entity.RequestStatus
+import com.mobileweb3.dvs.core.entity.validator.ValidatorVote
 
 @Composable
 fun ValidatorVotesContent(
@@ -24,29 +26,41 @@ fun ValidatorVotesContent(
         modifier = Modifier.padding(top = 24.dp),
         columns = GridCells.Fixed(1)
     ) {
-        val itemsCount = when (votesState.value!!.proposals) {
-            is RequestStatus.Data -> {
-                2
-            }
-            is RequestStatus.Error -> {
-                1
-            }
-            is RequestStatus.Loading -> {
-                1
-            }
-        }
-        items(itemsCount) {
-            when (votesState.value!!.proposals) {
-                is RequestStatus.Data -> {
-                    Text(text = "Data")
+        items(votesState.value!!.proposals.toViewItems()) {
+            when (it) {
+                ProposalViewItem.Loading -> {
+                    Text(text = "Loading...")
                 }
-                is RequestStatus.Error -> {
-                    Text(text = "Error")
+                ProposalViewItem.Error -> {
+                    Text(text = "Error...")
                 }
-                is RequestStatus.Loading -> {
-                    Text(text = "Loading")
+                is ProposalViewItem.Data -> {
+                    Text(text = "${it.validatorVote.proposal.title} : ${it.validatorVote.vote.name}")
                 }
             }
         }
     }
+}
+
+fun RequestStatus<List<ValidatorVote>>.toViewItems(): List<ProposalViewItem> {
+    return when (this) {
+        is RequestStatus.Loading -> {
+            listOf(ProposalViewItem.Loading, ProposalViewItem.Loading, ProposalViewItem.Loading, ProposalViewItem.Loading)
+        }
+        is RequestStatus.Error -> {
+            listOf(ProposalViewItem.Error)
+        }
+        is RequestStatus.Data -> {
+            data.map { ProposalViewItem.Data(it) }
+        }
+    }
+}
+
+sealed class ProposalViewItem {
+
+    object Loading : ProposalViewItem()
+
+    object Error : ProposalViewItem()
+
+    data class Data(val validatorVote: ValidatorVote) : ProposalViewItem()
 }
