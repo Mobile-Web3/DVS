7c5ef0b16781aaf323f61baa53fc3515fc1b9c00
qqdasdzxc <hook23@mail.ru>
2022-07-20 22:09:43 +0300

    ios: text shimmer view

---
+++ iosApp/iosApp/views/TextShimmer.swift
@@ -0,0 +1,25 @@
+//
+//  TextShimmer.swift
+//  DVS
+//
+//  Created by Dmitrii Kuzmin on 20.07.2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct TextShimmer: View {
+    var body: some View {
+        Text("loading card")
+            .foregroundColor(Color.white)
+            .background(Color.white)
+            .redacted(reason: .placeholder)
+            .shimmering()
+    }
+}
+
+struct TextShimmer_Previews: PreviewProvider {
+    static var previews: some View {
+        TextShimmer()
+    }
+}
