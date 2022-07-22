4034945f0e2fac895c853a8c29a552383587a204
qqdasdzxc <hook23@mail.ru>
2022-07-22 14:21:12 +0300

    shared: add ios redux extensions

--- shared/src/iosMain/kotlin/com/mobileweb3/dvs/app/IosReduxUtils.kt
+++ shared/src/iosMain/kotlin/com/mobileweb3/dvs/app/IosReduxUtils.kt
@@ -4,3 +4,12 @@ import com.mobileweb3.dvs.core.wrap
 
 fun ValidatorListStore.watchState() = observeState().wrap()
 fun ValidatorListStore.watchSideEffect() = observeSideEffect().wrap()
+
+fun ValidatorDetailsStore.watchState() = observeState().wrap()
+fun ValidatorDetailsStore.watchSideEffect() = observeSideEffect().wrap()
+
+fun ValidatorVotesStore.watchState() = observeState().wrap()
+fun ValidatorVotesStore.watchSideEffect() = observeSideEffect().wrap()
+
+fun SearchNetworkStore.watchState() = observeState().wrap()
+fun SearchNetworkStore.watchSideEffect() = observeSideEffect().wrap()
