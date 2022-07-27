bb062b65b1a8dd33342fae4674f7b8aa153a8dbb
qqdasdzxc <hook23@mail.ru>
2022-07-27 06:46:02 +0300

    ios: decompose voting header

---
+++ iosApp/iosApp/screens/voting/ValidatorVotingHeader.swift
@@ -0,0 +1,26 @@
+//
+//  ValidatorVotingHeader.swift
+//  DVS
+//
+//  Created by Dmitrii Kuzmin on 27.07.2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct ValidatorVotingHeader: View {
+    
+    var onButtonClick: () -> Void
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            DefaultButtonBack {
+                onButtonClick()
+            }
+            
+            Text("Note: click on proposal to see detailed information.\nClick on validator vote in top right to see the vote transaction")
+                .foregroundColor(Color.white)
+                .padding(.top, 8)
+        }
+    }
+}
