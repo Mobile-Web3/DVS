78ed4075b3ef80cb490f505ff0137750550f2ae5
qqdasdzxc <hook23@mail.ru>
2022-07-28 11:38:08 +0300

    ios: fix proposal votes view height and items spacing

--- iosApp/iosApp/screens/voting/ProposalVotesView.swift
+++ iosApp/iosApp/screens/voting/ProposalVotesView.swift
@@ -14,22 +14,23 @@ struct ProposalVotesView: View {
     var proposalPercentages: ProposalPercentages
     
     var body: some View {
-        HStack {
-            Text("")
-                .frame(width: proposalPercentages.yes, height: 50)
+        HStack(spacing: 0) {
+            Text("  ")
+                .frame(width: proposalPercentages.yes, height: 45)
                 .background(Color(UIColor.init(rgb: Int(Vote.yes.color))))
             
             Text("")
-                .frame(width: proposalPercentages.no, height: 50)
+                .frame(width: proposalPercentages.no, height: 45)
                 .background(Color(UIColor.init(rgb: Int(Vote.no.color))))
             
             Text("")
-                .frame(width: proposalPercentages.noWithVeto, height: 50)
+                .frame(width: proposalPercentages.noWithVeto, height: 45)
                 .background(Color(UIColor.init(rgb: Int(Vote.noWithVeto.color))))
             
             Text("")
-                .frame(width: proposalPercentages.abstain, height: 50)
+                .frame(width: proposalPercentages.abstain, height: 45)
                 .background(Color(UIColor.init(rgb: Int(Vote.abstain.color))))
         }
+        .cornerRadius(100)
     }
 }
