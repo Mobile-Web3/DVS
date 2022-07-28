2a9fa0aea32f83ddb90b524609ff218d1653686f
qqdasdzxc <hook23@mail.ru>
2022-07-28 11:25:26 +0300

    ios: make proposal votes view

---
+++ iosApp/iosApp/screens/voting/ProposalVotesView.swift
@@ -0,0 +1,35 @@
+//
+//  ProposalVotesView.swift
+//  DVS
+//
+//  Created by Dmitrii Kuzmin on 28.07.2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+import shared
+
+struct ProposalVotesView: View {
+    
+    var proposalPercentages: ProposalPercentages
+    
+    var body: some View {
+        HStack {
+            Text("")
+                .frame(width: proposalPercentages.yes, height: 50)
+                .background(Color(UIColor.init(rgb: Int(Vote.yes.color))))
+            
+            Text("")
+                .frame(width: proposalPercentages.no, height: 50)
+                .background(Color(UIColor.init(rgb: Int(Vote.no.color))))
+            
+            Text("")
+                .frame(width: proposalPercentages.noWithVeto, height: 50)
+                .background(Color(UIColor.init(rgb: Int(Vote.noWithVeto.color))))
+            
+            Text("")
+                .frame(width: proposalPercentages.abstain, height: 50)
+                .background(Color(UIColor.init(rgb: Int(Vote.abstain.color))))
+        }
+    }
+}
