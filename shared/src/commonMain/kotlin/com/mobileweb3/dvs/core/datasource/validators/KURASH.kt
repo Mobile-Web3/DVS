71b2604aa7abb934a0c7bf93235c1fd2653290f4
qqdasdzxc <hook23@mail.ru>
2022-08-05 09:03:17 +0300

    shared: add KURASH validator info

--- shared/src/commonMain/kotlin/com/mobileweb3/dvs/core/datasource/validators/KURASH.kt
+++ shared/src/commonMain/kotlin/com/mobileweb3/dvs/core/datasource/validators/KURASH.kt
@@ -1,16 +1,33 @@
 package com.mobileweb3.dvs.core.datasource.validators
 
+import com.mobileweb3.dvs.core.entity.validator.BlockchainNetwork
+import com.mobileweb3.dvs.core.entity.validator.Contact
 import com.mobileweb3.dvs.core.entity.validator.ValidatorModel
+import com.mobileweb3.dvs.core.entity.validator.ValidatorNetwork
 
 val KURASH = ValidatorModel(
     title = "KURASH",
     avatar = "https://user-images.githubusercontent.com/38581319/154812589-5af71295-27c0-45ca-9fea-b43278bed6bd.png",
     description = "",
-    mainNets = emptyList(),
+    mainNets = listOf(
+        ValidatorNetwork(
+            blockchainNetwork = BlockchainNetwork.FETCH_AI,
+            validatorAddress = "fetchvaloper14q3mtnec6wjyhyfxcjdgqmx3kqk52gwdryt4vz"
+        )
+    ),
     testNets = emptyList(),
     otherProjects = emptyList(),
     ambassadorPrograms = emptyList(),
     contributionsTypes = emptyList(),
-    contacts = emptyList(),
+    contacts = listOf(
+        Contact(
+            type = "telegram",
+            link = "https://t.me/AlexeySergeeich"
+        ),
+        Contact(
+            type = "github",
+            link = "https://github.com/kuraassh"
+        ),
+    ),
     otherInfo = emptyList()
 )
