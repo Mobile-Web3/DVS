90c922ca6bda8cf622e44645f08ab347969aba19
qqdasdzxc <hook23@mail.ru>
2022-08-09 10:08:54 +0300

    shared: parse validator status 2 as JAILED

--- shared/src/commonMain/kotlin/com/mobileweb3/dvs/core/entity/validator/ValidatorStatus.kt
+++ shared/src/commonMain/kotlin/com/mobileweb3/dvs/core/entity/validator/ValidatorStatus.kt
@@ -7,6 +7,7 @@ enum class ValidatorStatus {
     companion object {
         fun from(status: Int?): String {
             return when (status) {
+                2 -> "JAILED"
                 //seems like 3 is active
                 3 -> "ACTIVE"
                 null -> "UNKNOWN"
