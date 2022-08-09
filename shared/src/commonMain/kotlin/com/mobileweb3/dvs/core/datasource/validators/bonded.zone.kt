03f2c8b275ebc230192d94a615a70fa11c4017ec
qqdasdzxc <hook23@mail.ru>
2022-08-09 08:22:56 +0300

    shared: add info about bonded_zone except testnets and community contributions

--- shared/src/commonMain/kotlin/com/mobileweb3/dvs/core/datasource/validators/bonded.zone.kt
+++ shared/src/commonMain/kotlin/com/mobileweb3/dvs/core/datasource/validators/bonded.zone.kt
@@ -1,6 +1,9 @@
 package com.mobileweb3.dvs.core.datasource.validators
 
 import com.mobileweb3.dvs.core.entity.validator.BlockchainNetwork
+import com.mobileweb3.dvs.core.entity.validator.Contact
+import com.mobileweb3.dvs.core.entity.validator.Contribution
+import com.mobileweb3.dvs.core.entity.validator.Contributions
 import com.mobileweb3.dvs.core.entity.validator.ValidatorModel
 import com.mobileweb3.dvs.core.entity.validator.ValidatorNetwork
 
@@ -10,15 +13,90 @@ val bonded_zone = ValidatorModel(
     description = "",
     mainNets = listOf(
         ValidatorNetwork(
+            blockchainNetwork = BlockchainNetwork.BITCANNA,
+            validatorAddress = "bcnavaloper1yyps9aer7ldm8tjayft68q8ay5l4nch0waf4yh",
+            walletAddress = "bcna1yyps9aer7ldm8tjayft68q8ay5l4nch0hqc47r"
+        ),
+        ValidatorNetwork(
+            blockchainNetwork = BlockchainNetwork.BITSONG,
+            validatorAddress = "bitsongvaloper1pkepjd307346zadmlxpvs9wqrghkx735v0ehge",
+            walletAddress = "bitsong1pkepjd307346zadmlxpvs9wqrghkx735dt97cy"
+        ),
+        ValidatorNetwork(
             blockchainNetwork = BlockchainNetwork.CHRONIC,
             validatorAddress = "chronicvaloper105rzlcracxe9r32h8flpn4kpw8waqueqfsne7g",
             walletAddress = "chronic105rzlcracxe9r32h8flpn4kpw8waqueqexd8c2"
+        ),
+        ValidatorNetwork(
+            blockchainNetwork = BlockchainNetwork.DESMOS,
+            validatorAddress = "desmosvaloper13has6wzwyfyuq6r9j48cwugl6ajqw44gv0fa6k",
+            walletAddress = "desmos13has6wzwyfyuq6r9j48cwugl6ajqw44gjzpfsy"
+        ),
+        ValidatorNetwork(
+            blockchainNetwork = BlockchainNetwork.RIZON,
+            validatorAddress = "rizonvaloper1jnjg9fl5hflwgjyu9vrht3j52nn68yjesxaqmr",
+            walletAddress = "rizon1jnjg9fl5hflwgjyu9vrht3j52nn68yjerzlpen"
         )
     ),
-    testNets = emptyList(),
     otherProjects = emptyList(),
     ambassadorPrograms = emptyList(),
-    contributionsTypes = emptyList(),
-    contacts = emptyList(),
+    contributionsTypes = listOf(
+        Contributions(
+            type = "Technical",
+            contributions = listOf(
+                Contribution(
+                    label = "State-Sync service",
+                    link = "https://sync.bonded.zone/"
+                ),
+                Contribution(
+                    label = "Guide - How to setup Klever validator",
+                    link = "https://bondedzone.medium.com/how-to-setup-klever-validator-ff58b502e64d"
+                ),
+                Contribution(
+                    label = "Guide - Configure State-Sync for the Rizon mainnet",
+                    link = "https://bondedzone.medium.com/configure-state-sync-for-the-rizon-mainnet-9e1fd8c05d1f"
+                )
+            )
+        ),
+        Contributions(
+            type = "Group admin",
+            contributions = listOf(
+                Contribution(
+                    label = "Rizon ru",
+                    link = "https://t.me/rizon_atolo_ru"
+                ),
+                Contribution(
+                    label = "Транслятор Экосистемы Космос",
+                    link = "https://t.me/CosmosEcosystemNews_ru"
+                )
+            )
+        )
+    ),
+    contacts = listOf(
+        Contact(
+            type = "website",
+            link = "https://bonded.zone/"
+        ),
+        Contact(
+            type = "medium",
+            link = "https://bondedzone.medium.com/"
+        ),
+        Contact(
+            type = "twitter",
+            link = "https://twitter.com/SKosjakoff"
+        ),
+        Contact(
+            type = "telegram",
+            link = "https://t.me/kosjakoff"
+        ),
+        Contact(
+            type = "discord",
+            link = "https://discord.com/users/864778911548309506"
+        ),
+        Contact(
+            type = "keybase",
+            link = "https://keybase.io/kosjakoff"
+        )
+    ),
     otherInfo = emptyList()
 )
