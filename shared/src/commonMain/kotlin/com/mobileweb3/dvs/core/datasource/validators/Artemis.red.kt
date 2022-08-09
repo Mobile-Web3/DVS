e20f2807e80f1fae9a0699a40ebce98558cf6b94
qqdasdzxc <hook23@mail.ru>
2022-08-09 07:56:25 +0300

    shared: fill info about Artemis.red

--- shared/src/commonMain/kotlin/com/mobileweb3/dvs/core/datasource/validators/Artemis.red.kt
+++ shared/src/commonMain/kotlin/com/mobileweb3/dvs/core/datasource/validators/Artemis.red.kt
@@ -1,16 +1,34 @@
 package com.mobileweb3.dvs.core.datasource.validators
 
+import com.mobileweb3.dvs.core.entity.validator.BlockchainNetwork
+import com.mobileweb3.dvs.core.entity.validator.Contact
 import com.mobileweb3.dvs.core.entity.validator.ValidatorModel
+import com.mobileweb3.dvs.core.entity.validator.ValidatorNetwork
 
 val Artemis_red = ValidatorModel(
     title = "Artemis.red",
     avatar = "https://user-images.githubusercontent.com/38581319/166976374-e8aa7581-df5a-4fed-bb7e-3d170bd4af6c.png",
-    description = "",
-    mainNets = emptyList(),
+    description = "More nets going soon...",
+    mainNets = listOf(
+        ValidatorNetwork(
+            blockchainNetwork = BlockchainNetwork.SECRET,
+            validatorAddress = "secretvaloper1jjlfeshltdy6ngnf5tg6aeaygsvf0kxg8wck04",
+            walletAddress = "secret1jjlfeshltdy6ngnf5tg6aeaygsvf0kxgkfktzu"
+        )
+    ),
     testNets = emptyList(),
     otherProjects = emptyList(),
     ambassadorPrograms = emptyList(),
     contributionsTypes = emptyList(),
-    contacts = emptyList(),
+    contacts = listOf(
+        Contact(
+            type = "github",
+            link = "https://github.com/Artemisred"
+        ),
+        Contact(
+            type = "telegram",
+            link = "https://t.me/Art3misred"
+        )
+    ),
     otherInfo = emptyList()
 )
